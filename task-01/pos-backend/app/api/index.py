cdae14e69f9575cfe4c13fa57fd8e784fb32e4bd
T M B M Thennakoon <142225271+Bhanuka2083@users.noreply.github.com>
2026-09-14 19:02:34 +0530

    Fix import statement for app module

--- task-01/pos-backend/app/api/index.py
+++ task-01/pos-backend/app/api/index.py
@@ -1,7 +1,6 @@
 import os
 import sys
 
-# Ensure Python can find the 'app' module from the parent directory
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from app.main import app
